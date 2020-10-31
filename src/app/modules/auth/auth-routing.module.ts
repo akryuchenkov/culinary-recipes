d3018a2b5532509132e217dd381102b8737ee8df
a.kryuchenkov <a.kryuchenkov2@gmail.com>
2020-10-31 20:24:58 +0300

    add global routes

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {RegisterPageComponent} from './pages/register-page/register-page.component';
 import {AuthLayoutComponent} from './layouts/auth-layout/auth-layout.component';
+import {routes as routeNames} from '../../routes';
 
 const routes: Routes = [
   {
@@ -10,11 +11,11 @@ const routes: Routes = [
     component: AuthLayoutComponent,
     children: [
       {
-        path: 'login',
+        path: routeNames.auth.login,
         component: LoginPageComponent
       },
       {
-        path: 'registration',
+        path: routeNames.auth.registration,
         component: RegisterPageComponent
       }
     ]
--- src/app/modules/auth/layouts/auth-layout/auth-layout.component.ts
+++ src/app/modules/auth/layouts/auth-layout/auth-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {routes} from '../../../../routes';
 
 @Component({
   selector: 'app-auth-layout',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthLayoutComponent implements OnInit {
 
-  constructor() { }
+  routes = routes;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
